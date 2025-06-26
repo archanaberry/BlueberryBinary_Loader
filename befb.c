66d6082f62f50e6db0d1a5abb030776ce678c012
Archana Berry OPS!!! <archanaberry101originally@gmail.com>
2025-06-26 09:28:48 +0700

    Create befb.c

---
+++ befb.c
@@ -0,0 +1,19 @@
+/**************************************************************
+ *    Archana Berry OS - Project Source Code                  *
+ *                                                            *
+ *    Filename   : befb.h                                     *
+ *    Component  : BEFB Loadee Core                           *
+ *    Description: Maker file BEFB binary and segment map     *
+ *                                                            *
+ *    Copyright © 2025 Archana Berry Project OS Devs          *
+ *    Licensed under ABRL v1.0 - Research License             *
+ *                                                            *
+ *    You may not use this file except in compliance with     *
+ *    the License.                                            *
+ *                                                            *
+ *    Unless required by applicable law or agreed to in       *
+ *    writing, software distributed under the License is      *
+ *    distributed on an "AS IS" BASIS, WITHOUT WARRANTIES     *
+ *    OR CONDITIONS OF ANY KIND, either express or implied.   *
+ **************************************************************/
+
